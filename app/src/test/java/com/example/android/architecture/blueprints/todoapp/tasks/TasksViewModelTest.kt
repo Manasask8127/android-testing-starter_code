566e981a29f79e02fadf20edc4e914ecbc07ff66
Manasask8127 <manasask8127@gmail.com>
2022-06-08 00:32:48 +0530

    basic livedata testing

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -1,14 +1,23 @@
 package com.example.android.architecture.blueprints.todoapp.tasks
 
 import android.app.Application
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import getOrAwaitValue
 import junit.framework.TestCase
+import org.hamcrest.CoreMatchers.not
+import org.hamcrest.CoreMatchers.nullValue
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class TasksViewModelTest{
+
+    @get:Rule
+    val instantTaskExecutable=InstantTaskExecutorRule()
 
     @Test
     fun addNewTask_setsNewTaskEvent(){
@@ -19,7 +28,8 @@ class TasksViewModelTest{
         taskViewModel.addNewTask()
 
         // Then the new task event is triggered
-
+        val value = taskViewModel.newTaskEvent.getOrAwaitValue()
+        assertThat(value.getContentIfNotHandled(), (not(nullValue())))
 
     }
 }
